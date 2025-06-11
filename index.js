20a89a7d8bcf49f716e37c6235048d6fce24d94b
alex kamau <alex.kamau1@student.moringaschool.com>
2025-06-10 22:27:27 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+//function finding matching list of drivers
+function findMatching(drivers,searchName){
+    return drivers.filter(driver => driver.toLowerCase() === searchName.toLowerCase());
+}
+
+//function finding names with provided letters
+function fuzzyMatch(drivers,searchName){
+    return drivers.filter(driver => driver.toLowerCase().startsWith(searchName.toLowerCase()))
+
+}
+
+//function finding name that matches drivers hometown
+function matchName(drivers,searchName){
+    return drivers.filter(driver => driver.name === searchName);
+
+}
+
+const drivers = ['Sammy', 'Sarah', 'Sally', 'Annette'];
+fuzzyMatch(drivers, 'Sa');
+
+module.exports = {
+  findMatching,
+  fuzzyMatch,
+  matchName
+};
